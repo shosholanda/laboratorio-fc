3061a8e969082b0fdf75b0a63ade7baf1b511688
Davidshiro Pichu <davidshiro.pichu@ciencias.unam.mx>
2023-02-28 11:43:45 -0600

    Level 5 final

---
+++ Level5.java
@@ -0,0 +1,22 @@
+
+
+public class Level5 {
+	
+	/* Ordenar las letras de la cadena s con codificación ASCII en tiempo O(n) */
+	public static String ordena(String s){
+		return null;
+	}
+	
+	/** Level extremo: Reduce el código a 9 líneas o menos */
+	
+	public static void main (String[] args){
+		String s = "El cielo nocturno contiene más estrellas de las que puedo contar, aunque menos de cinco mil son visibles a simple vista. El universo observable contiene alrededor de setenta billones de estrellas. Pero el número de estrellas es finito, mientras que los números naturales son infinitos. Cuenta todas las estrellas y aún te quedarán tantos números naturales como al principio.";
+		
+		System.out.println("Cadena:\n" + s);
+		
+		s = ordena(s);
+		
+		System.out.println("Cadena ordenada:\n" + s);
+	}
+	
+}
